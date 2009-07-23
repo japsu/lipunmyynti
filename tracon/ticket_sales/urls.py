ed21c5120d4d0c57bef972e011d95ae3f0ae63ac
Santtu Pajukanta <santtu@pajukanta.fi>
2009-07-24 00:39:49 +0300

    view structure

--- tracon/ticket_sales/urls.py
+++ tracon/ticket_sales/urls.py
@@ -1,7 +1,17 @@
+# encoding: utf-8
+# vim: shiftwidth=4 expandtab
+
 from django.conf.urls.defaults import *
 
 from tracon.ticket_sales.views import *
 
 urlpatterns = patterns('',
-    url(r'^', test_view, name="test_view")
+    url(r'^welcome/$', welcome_view, name="welcome_view"),
+    url(r'^tickets/$', tickets_view, name="tickets_view"),
+    url(r'^shirts/$', shirts_view, name="shirts_view"),
+    url(r'^address/$', address_view, name="address_view"),
+    url(r'^confirm/$', confirm_view, name="confirm_view"),
+    url(r'^thanks/$', thanks_view, name="thanks_view"),
+
+    url(r'^', welcome_view, name="empty_url")
 )
