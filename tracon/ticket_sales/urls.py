238340460c52d498162d0790268b0cf1b7dc4532
Santtu Pajukanta <santtu@pajukanta.fi>
2009-11-22 18:02:14 +0200

    äsf

--- tracon/ticket_sales/urls.py
+++ tracon/ticket_sales/urls.py
@@ -7,12 +7,12 @@ from django.views.generic.simple import redirect_to
 from tracon.ticket_sales.views import *
 
 urlpatterns = patterns('',
-    url(r'^tervetuloa/$', welcome_view, name="welcome_phase"),
-    url(r'^liput/$', tickets_view, name="tickets_phase"),
-    url(r'^paidat/$', shirts_view, name="shirts_phase"),
-    url(r'^toimitusosoite/$', address_view, name="address_phase"),
-    url(r'^vahvistus/$', confirm_view, name="confirm_phase"),
-    url(r'^kiitos/$', thanks_view, name="thanks_phase"),
+    url(r'tervetuloa/$', welcome_view, name="welcome_phase"),
+    url(r'liput/$', tickets_view, name="tickets_phase"),
+    url(r'paidat/$', shirts_view, name="shirts_phase"),
+    url(r'toimitusosoite/$', address_view, name="address_phase"),
+    url(r'vahvistus/$', confirm_view, name="confirm_phase"),
+    url(r'kiitos/$', thanks_view, name="thanks_phase"),
 
-    url(r'^/$', redirect_to, dict(url='/tervetuloa/'), name="empty_url"),
+    url(r'$', redirect_to, dict(url='/tervetuloa/'), name="empty_url"),
 )
