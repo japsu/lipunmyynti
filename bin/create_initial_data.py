9718f9a2f1f014d2a83d782e918bf5d8f0af1c1f
Santtu Pajukanta <santtu@pajukanta.fi>
2009-12-06 19:48:14 +0200

    tommottia

--- bin/create_initial_data.py
+++ bin/create_initial_data.py
@@ -2,12 +2,12 @@
 # encoding: utf-8
 # vim: shiftwidth=4 expandtab
 
-from tracon.ticket_sales.models import ShirtSize
+from tracon.ticket_sales.models import *
 
-BASE_SIZES = ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]
+BASE_SIZES = ["XS", "S", "M", "L", "XL", "XXL", "3XL", "4XL", "5XL"]
 
 NORMAL_NOT_AVAILABLE = ["XS"]
-LADYFIT_NOT_AVAILABLE = ["XXL", "XXXL"]
+LADYFIT_NOT_AVAILABLE = ["3XL", "4XL", "5XL"]
 
 def create_shirt_sizes():
     # Create normal sizes
@@ -22,5 +22,18 @@ def create_shirt_sizes():
         obj = ShirtSize(name=size, ladyfit=True, available=available)
 	obj.save()
 
+PRODUCTS = [
+    ("Ennakkolippu ja T-paita", 2200),
+    ("Ennakkolippu, T-paita ja lattiamajoitus", 3200),
+    ("Ennakkolippu ja lattiamajoitus", 1900),
+    ("Pelkkä ennakkolippu", 900),
+]
+
+def create_products():
+    for name, price_cents in PRODUCTS:
+        obj = Product(name=name, price_cents=price_cents)
+        obj.save()
+
 if __name__ == "__main__":
     create_shirt_sizes()
+    create_products()
