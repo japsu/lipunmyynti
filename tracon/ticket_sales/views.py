030f11dfd14386095914b41f5cec35904128777b
Santtu Pajukanta <japsu@tracon.fi>
2010-07-01 01:40:07 +0300

    suljin lippukaupan

--- tracon/ticket_sales/views.py
+++ tracon/ticket_sales/views.py
@@ -36,6 +36,7 @@ __all__ = [
     "cancel_batch_view",
     "deliver_batch_view",
     "search_view",
+    "closed_view",
 ]    
 
 FIRST_PHASE = "welcome_phase"
@@ -367,7 +368,26 @@ class ThanksPhase(Phase):
 
         return redirect(self.next_phase)
 
+class ClosedPhase(Phase):
+    name = "welcome_phase"
+    friendly_name = "Tervetuloa!"
+    template = "ticket_sales/closed.html"
+    prev_phase = None
+    next_phase = None
+    can_cancel = True
+    index = 0
+
+    def available(self, request):
+        return True
+
+    def save(self, request, form):
+        pass
+
+    def next(self, request):
+        return HttpResponseRedirect("http://2010.tracon.fi")
+
 thanks_view = ThanksPhase()
+closed_view = ClosedPhase()
 
 ALL_PHASES = [welcome_view, tickets_view, shirts_view, address_view, confirm_view, thanks_view]
 for num, phase in enumerate(ALL_PHASES):
