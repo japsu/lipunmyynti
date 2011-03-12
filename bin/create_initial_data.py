9fecb2cca37c8b33f34814a5c0dcd8c17216dd2b
Santtu Pajukanta <japsu@tracon.fi>
2011-03-12 02:15:58 +0200

    Valmistaudutaan mahdolliseen pe-la-yön majoitukseen.

--- bin/create_initial_data.py
+++ bin/create_initial_data.py
@@ -5,19 +5,21 @@
 from tracon.ticket_sales.models import *
 
 PRODUCTS = [
-    ("Koko viikonlopun lippu", 1500, True),
-    ("Lauantailippu", 1000, True),
-    ("Sunnuntailippu", 1000, True),
-    ("Lattiamajoitus lauantain ja sunnuntain väliseksi yöksi", 700, False),
-    ("Taidekujapöytä", 1500, False)
+    ("Koko viikonlopun lippu", 1500, True, True),
+    ("Lauantailippu", 1000, True, True),
+    ("Sunnuntailippu", 1000, True, True),
+    ("Lattiamajoitus perjantain ja lauantain väliseksi yöksi", 700, False, False),
+    ("Lattiamajoitus lauantain ja sunnuntain väliseksi yöksi", 700, False, True),
+    ("Taidekujapöytä", 1500, False, True)
 ]
 
 def create_products():
-    for name, price_cents, requires_shipping in PRODUCTS:
+    for name, price_cents, requires_shipping, available in PRODUCTS:
         obj = Product(
             name=name,
             price_cents=price_cents,
-            requires_shipping=requires_shipping
+            requires_shipping=requires_shipping,
+            available=available
         )
         obj.save()
 
