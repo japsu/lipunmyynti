d734d6cafadb8ce0a0341f20116f626e8eca1ad8
Santtu Pajukanta <japsu@tracon.fi>
2011-04-15 17:27:36 +0300

    tuotantoon

--- bin/create_initial_data.py
+++ bin/create_initial_data.py
@@ -6,14 +6,14 @@ from tracon.ticket_sales.models import *
 
 PRODUCTS = [
     # (name, price_cents, description, classname, requires_shipping, sell_limit, available)
-    ("Koko viikonlopun lippu", 1500, "Koko viikonlopun lippu on voimassa ovien avautumisesta lauantaiaamuna tapahtuman loppuun sunnuntai-iltana. Vain koko viikonlopun lipulla pääset nauttimaan yön tunnelmista Traconissa!", "i_viikonloppu.png", "large", True, 5000, True, None),
-    ("Lauantailippu", 1000, "Lauantailippu on voimassa lauantaina kello 10-20.", "i_lauantai.png", "medium", True, 5000, True, None),
-    ("Sunnuntailippu", 1000, "Sunnuntailippu on voimassa sunnuntaina kello 08-19.", "i_sunnuntai.png", "medium", True, 5000, True, None),
+    ("Koko viikonlopun lippu", 1500, "Koko viikonlopun lippu on voimassa ovien avautumisesta lauantaiaamuna tapahtuman loppuun sunnuntai-iltana. Vain koko viikonlopun lipulla pääset nauttimaan yön tunnelmista Traconissa!", "i_viikonloppu.png", "large", True, 5500, True, None),
+    ("Lauantailippu", 1000, "Lauantailippu on voimassa lauantaina kello 10-20.", "i_lauantai.png", "medium", True, 3000, True, None),
+    ("Sunnuntailippu", 1000, "Sunnuntailippu on voimassa sunnuntaina kello 08-18.", "i_sunnuntai.png", "medium", True, 3000, True, None),
     ("Lattiamajoitus perjantain ja lauantain väliseksi yöksi", 700, "Majoitu kätevästi ja edullisesti Traconin lattiamajoituksessa tapahtumapaikan lähellä! Huomaathan, että majoitusvaraus on voimassa vain yhdessä viikonloppu- tai lauantailipun kanssa. Tarvitset oman makuupussin ja -alustan tai patjan.", "i_pela.png", "small", False, 500, False,
 
 """Lisätietoja lattiamajoituksesta lähetetään sähköpostitse lähempänä
 tapahtumaa."""),
-    ("Lattiamajoitus lauantain ja sunnuntain väliseksi yöksi", 700, "Majoitu kätevästi ja edullisesti Traconin lattiamajoituksessa tapahtumapaikan lähellä! Huomaathan, että majoitusvaraus on voimassa vain yhdessä viikonloppu-, lauantai- tai sunnuntailipun kanssa. Tarvitset oman makuupussin ja -alustan tai patjan.", "i_lasu.png", "small", False, 500, True,
+    ("Lattiamajoitus lauantain ja sunnuntain väliseksi yöksi", 700, "Majoitu kätevästi ja edullisesti Traconin lattiamajoituksessa tapahtumapaikan lähellä! Huomaathan, että majoitusvaraus on voimassa vain yhdessä viikonloppu-, lauantai- tai sunnuntailipun kanssa. Tarvitset oman makuupussin ja -alustan tai patjan.", "i_lasu.png", "small", False, 200, True,
 
 """Lisätietoja lattiamajoituksesta lähetetään sähköpostitse lähempänä
 tapahtumaa."""),
--- bin/nukedb.sh
+++ bin/nukedb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Ei nuketeta tietokantaa."
+exit 1
+
 set -e
 sudo -u postgres dropdb traconkauppa
 sudo -u postgres createdb -O traconkauppa -E UNICODE traconkauppa
