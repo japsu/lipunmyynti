7ad7c1a4f5e4ca45f3e4a5d7cc1739f39aac2494
Santtu Pajukanta <japsu@tracon.fi>
2011-08-11 13:22:07 +0300

    karhua kaikkia tilauksia, ei vain majoituksia

--- bin/send_payment_reminders.py
+++ bin/send_payment_reminders.py
@@ -5,13 +5,13 @@
 from tracon.ticket_sales.models import Order
 from datetime import  datetime, timedelta
 
-DRY_RUN = False
+DRY_RUN = True
 THRESHOLD = datetime.now() - timedelta(days=2)
 
 def get_overdue_orders():
     # XXX iterates over all orders, baaad
     for order in Order.objects.all():
-        if order.is_confirmed and not order.requires_shipping and not order.is_paid and order.due_date < THRESHOLD:
+        if order.is_confirmed and not order.is_paid and order.due_date < THRESHOLD:
             yield order
 
 def send_payment_reminders():
