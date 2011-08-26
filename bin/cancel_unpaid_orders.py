3f18536db6aadcfc3dd45988e86366b3340fe898
Santtu Pajukanta <japsu@tracon.fi>
2011-08-27 00:22:09 +0300

    PURKKA: majoituksen erapaivajuttuja

--- bin/cancel_unpaid_orders.py
+++ bin/cancel_unpaid_orders.py
@@ -7,11 +7,12 @@ from datetime import  datetime, timedelta
 
 DRY_RUN = False
 THRESHOLD = datetime.now() - timedelta(days=2)
+DO_NOT_TOUCH = set((2,3,9,120,59,74,151,207,227,235,237,260,264,270,292,299,381))
 
 def get_cancellable_orders():
     # XXX iterates over all orders, baaad
     for order in Order.objects.all():
-        if order.is_confirmed and order.requires_shipping and not order.is_paid and order.is_overdue and not order.is_batched:
+        if order.is_confirmed and not order.is_paid and order.is_overdue and not order.is_batched and not order.is_cancelled and order.id not in DO_NOT_TOUCH:
             yield order
 
 def send_payment_reminders():
