f72be1ec96699c617bda784b02e1f39587f9eab6
Santtu Pajukanta <japsu@tracon.fi>
2011-08-28 23:01:26 +0300

    schoolification continues

--- bin/schoolificate.py
+++ bin/schoolificate.py
@@ -6,8 +6,10 @@ from tracon.ticket_sales.models import *
 from send_sleeping_info import CLASU, TAMMERKOSKI, KAUKAJARVI, SLEEPY_PRODUCTS
 from create_schools import KAUKKIS, PERUS
 
-def schoolificate():
-    orders = Order.objects.filter(
+DRY_RUN=True
+
+def get_sleepy_order_set():
+    return Order.objects.filter(
         # Is confirmed
         confirm_time__isnull=False,
 
@@ -18,12 +20,30 @@ def schoolificate():
         cancellation_time__isnull=True,
 
         # Contains lodging
-        order_product_set__in=SLEEPY_PRODUCTS
+        order_product_set__in=SLEEPY_PRODUCTS,
 
+        # Has not been schoolificated yet
+        school__isnull=True
     # First come, sleep closer
     ).order_by("confirm_time")
 
-    print orders.count()
+def schoolificate(orders):
+    for order in orders:
+        sleepy_op = order.order_product_set.get(product__in=SLEEPY_PRODUCTS)
+
+        possible_schools = sleepy_op.product.school_set.all().order_by("-priority")
+
+        for school in possible_schools:
+            if sleepy_op.count < school.amount_available:
+                order.school = school
+                break
+
+        if order.school:
+            print order, sleepy_op.count, order.school
+            if not DRY_RUN:
+                order.save()
+        else:
+            print order, sleepy_op.count, "FAILED TO SCHOOLIFICATE"
 
 if __name__ == "__main__":
-    schoolificate()
+    schoolificate(get_sleepy_order_set())
