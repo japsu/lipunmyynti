9192ee3ec7858d5956f18c3a99b68c61a024eaa8
Santtu Pajukanta <santtu@pajukanta.fi>
2013-05-02 18:46:53 +0300

    restore admin.py

---
+++ ticket_sales/admin.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+# vim: shiftwidth=4 expandtab
+
+from ticket_sales.models import *
+from django.contrib import admin
+
+class CustomerInline(admin.StackedInline):
+    model = Customer
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [
+        OrderProductInline,
+#        CustomerInline
+    ]
+
+admin.site.register(Order, OrderAdmin)
+
+for cls in (School, Batch, Product, Customer):
+    admin.site.register(cls)
